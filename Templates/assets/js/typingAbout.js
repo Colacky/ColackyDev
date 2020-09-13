9f4764c3224bfe8f58b95b5417e2f5d5915c28f5
Colacky <51408423+Colacky@users.noreply.github.com>
2020-09-13 19:26:55 +0200

    About page done
    Finished the basic outline for the about page along with a go back button leading to back to index

--- Templates/assets/js/typingAbout.js
+++ Templates/assets/js/typingAbout.js
@@ -1,5 +1,5 @@
 var i = 0;
-var txt = "Hello. I'm glad to have you here. \nMy name is Sebastian 'Colacky' Kołacki and I'm a self-taught freelance programmer. \nI've made this website using HTML, Javascrip, jQuery and CSS. \nI've used various resources to learn including w3schools, stackoverflow, youtube and official documentation. \n My skills: \n>Python\n>Django\n>HTML\n>CSS\n>Javascript\nI'm always learning new things and expanding my skillset.\nI'm'well versed in both corporate and non-corporate environments.\n"; /* Text to be typed */
+var txt = "Hello. I'm glad to have you here. \nMy name is Sebastian 'Colacky' Kołacki and I'm a self-taught freelance programmer. \nI've made this website using HTML, Javascrip, jQuery and CSS. \nI've used various resources to learn including w3schools, stackoverflow, youtube and official documentation. \nMy programming-related skills: \n >Python\n >Django\n >HTML\n >CSS\n >Javascript\nI'm always learning new things and expanding my skillset.\nI'm well versed in both corporate and non-corporate environments.\nI also have experience in the finance/administration sector and possess strong interpersonal skills, always setting the bar high."; /* Text to be typed */
 var speed = 40; /* Duration of typing effect in milliseconds */
 
 function typeText1() {
@@ -8,5 +8,6 @@ function typeText1() {
         i++;
         setTimeout(typeText1, speed)
     }
+    document.getElementById("about").disabled = true;
 }
 
